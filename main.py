7b8f02047f5a6956a94935fe4013eb55af46a802
David Glez <david@Davids-MacBook-Pro.local>
2023-09-24 11:12:20 -0600

    Ultimos cambios

--- main.py
+++ main.py
@@ -36,6 +36,18 @@ loaders = [UnstructuredPDFLoader(os.path.join(pdf_folder_path, fn)) for fn in os
 index = VectorstoreIndexCreator().from_loaders(loaders)
 
 
-print(index.query("Que es la calificacion en los fondos de inversion"))
+pregunta1 = "Soy recién egresado de mi carrera y el mundo de las inversionas me llama. Pero no tengo nada de información, hay algo que me puedas decir sobre esto en Banorte?"
+pregunta2 = "¿Qué es la calificación en los fondos de inversion?"
+
+
+pregunta3 = "¿Me puedes decir qué es el horizonte de inversion?"
+pregunta4 = "Me puedes decir de qué trata el fondo de inversión Dolares+ de Banorte"
+pregunta5 = "Me gustaria saber cuales son los mejores planes de inversion de Banorte para alguien con mucho capital, tolerante a los largos plazos y que acepta correr riesgos con su inversion"
+pregunta6 = "Que plan de Banorte para invertir es para mi si quisiera empezar con poco capital y esperar un pequeño plazo. Tampoco me gustaria correr riesgos"
+pregunta7 = "Me gustaria saber cuales son los mejores planes de inversion de Banorte para un recien egresado de la unviersidad con poco capital que busca crecer su dinero"
+
+
+
+print(index.query(pregunta2))
 
 
